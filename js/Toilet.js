f79ba3e3ae6373298677593184d49fd0231467d8
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2023-04-26 12:32:08 +0200

    multiple markers

---
+++ js/Toilet.js
@@ -0,0 +1,8 @@
+export default class Toilet{
+    constructor(latitude,longtitude,adress){
+        this.latitude = latitude;
+        this.longtitude = longtitude
+        this.adress = adress;
+    }
+    
+}
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 "use strict";
+import Toilet from "./Toilet.js";
 
 // de leaflet library is reeds geimporteerd, en beschikbaar als "L"
 // dit via de script en css tag in de index.html, en de "map" div die werd toegevoegd.
@@ -7,14 +8,15 @@
 const app = {
     map: null, // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
     init() {
+        this.loadMarkers();
         // initialise de kaart
-        var map = L.map('map').setView([50.8360171,4.3353876], 13);
+     app.map = L.map('map').setView([50.8360171,4.3353876], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Contributors'
-}).addTo(map);
+}).addTo(app.map);
 
-L.marker([50.8423185, 4.3223651]).addTo(map)
+L.marker([50.8423185, 4.3223651]).addTo(app.map)
     .bindPopup('EHB <br> campus kaai')
     .openPopup();
 
@@ -23,14 +25,26 @@ L.marker([50.8423185, 4.3223651]).addTo(map)
     },
     loadMarkers() {
 
-        fetch('https://bruxellesdata.opendatasoft.com/api/records/1.0/search/?dataset=atms&q=')
+        fetch('https://opendata.brussels.be/api/records/1.0/search/?dataset=toiletten&q=&rows=100&geofilter.distance=50.846475%2C+4.352793%2C+5000')
+        .then(toilet => toilet.json())
+        .then(function(data){
+            data.records.forEach(function(toiletInfo){
+                const toilet = new Toilet(toiletInfo.fields.wgs84_lat,toiletInfo.fields.wgs84_long,toiletInfo.fields.adrvoisfr);
+                console.log(toilet);
+                app.addMarker(toilet.latitude,toilet.longtitude, toilet.adress);
+            });
+           
+        })
         // fetch de data van opendata.brussels.be
             // als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
+          
         
         
     },
-    addMarker(lat, lon) {
-        // voeg een marker toe op lat, lon
+    addMarker(lat, lon, adress) {
+        L.marker([lat, lon]).addTo(app.map)
+        .bindPopup(adress)
+        .openPopup();
     }
 }
 
