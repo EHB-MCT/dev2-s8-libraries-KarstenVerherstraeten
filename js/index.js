a89fb3642ca5cefeaeaffc838a4fa35e81a18ff0
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2023-04-26 10:49:54 +0200

    static marker done

--- js/index.js
+++ js/index.js
@@ -8,14 +8,22 @@ const app = {
     map: null, // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
     init() {
         // initialise de kaart
+        var map = L.map('map').setView([50.8360171,4.3353876], 13);
 
-        // voeg een tile layer toe, met URL https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-        // vergeet openstreetmap attributie niet
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Contributors'
+}).addTo(map);
+
+L.marker([50.8423185, 4.3223651]).addTo(map)
+    .bindPopup('EHB <br> campus kaai')
+    .openPopup();
+
         
-        // gebruik de functie "loadMarkers" om de markers toe te voegen
+        
     },
     loadMarkers() {
 
+        fetch('https://bruxellesdata.opendatasoft.com/api/records/1.0/search/?dataset=atms&q=')
         // fetch de data van opendata.brussels.be
             // als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
         
